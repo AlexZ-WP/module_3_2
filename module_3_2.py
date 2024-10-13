4c5e6c4379785631dd1799c18807ac1b03c13980
AlexZ-WP <alexz10@mail.ru>
2024-10-13 20:30:23 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    # sender.endswith('.com') and sender.endswith('.ru') and sender.endswith('.net') is True
+    # recipient.endswith('.com') and recipient.endswith('.com') and recipient.endswith('.com') is True
+    # if sender.endswith('') and recipient.endswith('') is False:
+    #     print("Невозможно отправить письмо с адреса ", sender, 'на адрес ', recipient)
+
+    if '@' not in sender[:0] and '@' not in recipient[0:]:
+        print("Невозможно отправить письмо с адреса ", sender, 'на адрес ', recipient)
+
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender != "university.help@gmail.com":
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ", sender, "на адрес ", recipient)
+
+    else:
+        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
